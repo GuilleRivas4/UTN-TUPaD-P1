18e4a764b6958ca9815d87001dd7441a3740ddc6
Guille Rivas <a.guille.rivas@gmail.com>
2025-05-27 20:26:53 -0300

    Trabajo Practico Numero 5 realizado

--- 05 Funciones/Rivas-Alfredo-Guillermo-Tp5.py
+++ 05 Funciones/Rivas-Alfredo-Guillermo-Tp5.py
@@ -1,83 +1,112 @@
-# 1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+############################################### Funciones ###############################################
 
-""" lista_uno_cien = list(range(0,101,4))
-print (lista_uno_cien) """
+""" 1. 
+def imprimir_hola_mundo():
+    print("Hola Mundo!") """
 
-""" 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
-"""
+""" 2.
+def saludar_usuario(nombre):
+    print(f"Hola {nombre}!") """
 
-""" punto_dos = [1,2,3,4,5]
-print (puntoDos[-2]) """
+""" 3.
+def  informacion_personal(nombre, apellido,edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}") """
 
-""" 3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por ejemplo: lista_vacia = [] """
+""" 4.
+def calcular_area_circulo(radio):
+    print(3.1416 * (radio*radio)) 
 
-""" lista_vacia = []
-lista_vacia.append("Hola")
-lista_vacia.append("como")
-lista_vacia.append("estas?")
-print (lista_vacia) """
+def calcular_perimetro_circulo(radio):
+    print(2 * 3.1416 * radio)  """
 
-""" 4) Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos! 
-animales = ["perro", "gato", "conejo", "pez"]
-"""
-""" animales = ["perro", "gato", "conejo", "pez"]
-animales[1] = "loro"
-animales[-1] = "oso"
-print (animales) """
+""" 5.
+def segundos_a_horas(segundos):
+    print(segundos/3600) """
+
+""" 6.
+def tabla_multiplicar(numero):
+    for i in range(1,11):
+        print(numero*i) """
+
+""" 7.
+def operaciones_basicas(a, b):
+    suma = a + b
+    resta = a - b
+    multiplicacion = a * b
+    division = a / b if b != 0 else "Error: División por cero"
+    
+    return (suma, resta, multiplicacion, division) """
+
+""" 8.
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc """
+
+""" 9.
+def celsius_a_fahrenheit(celsius):
+    print(f"La temperatura en fahrenheit es: {celsius * 9 / 5 + 32}") """
+
+""" 10.
+def calcular_promedio(a, b, c):
+    print(f"El promedio de los 3 numeros es: {(a + b + c) / 3}") """
+
+############################################### Programa principal ###############################################
+
+""" 1. 
+imprimir_hola_mundo() """
+
+""" 2.
+saludar_usuario("Marcos") """
+
+""" 3.
+nombre = input("Ingrese su nombre")
+apellido = input("Ingrese su apellido")
+edad = int(input("Ingrese su edad"))
+residencia = input("Ingrese su residencia")
+informacion_personal(nombre,apellido,edad,residencia) """
+
+""" 4.
+radio = int(input("Ingrese el radio del circulo"))
+calcular_area_circulo(radio)
+calcular_perimetro_circulo(radio) """
+
+""" 5.
+segundos = int(input("Ingrese la cantidad de segundos"))
+segundos_a_horas(segundos) """
+
+""" 6. 
+numero = int(input("Ingrese un numero para conocer su tabla"))
+tabla_multiplicar(numero) """
+
+""" 7.
+num1 = int(input("Ingrese un numero"))
+num2 = int(input("Ingrese otro numero"))
+
+resultados = operaciones_basicas(num1, num2)
+print(f"Suma: {resultados[0]}")
+print(f"Resta: {resultados[1]}")
+print(f"Multiplicación: {resultados[2]}")
+print(f"División: {resultados[3]}") """
+
+""" 8.
+peso = float(input("Ingrese su peso en kilogramos: "))
+altura = float(input("Ingrese su altura en metros: "))
+
+imc = calcular_imc(peso, altura)
+
+print(f"Su IMC es: {imc:.2f}") """
+
+""" 9.
+celsius = int(input("Ingrese la temperatura en celsius "))
+celsius_a_fahrenheit(celsius) """
+
+""" 10.
+num1 = int(input("Ingrese el primer numero"))
+num2 = int(input("Ingrese el segundo numero"))
+num3 = int(input("Ingrese el tercer numero"))
+
+calcular_promedio(num1,num2,num3) """
 
 
-""" 5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
-numeros = [8, 15, 3, 22, 7]
-numeros.remove(max(numeros))
-print(numeros)
-"""
-""" El programa crea una lista con 5 valores numericos. Luego elimina el valor numerico mas "grande". Por ultimo muestra la lista actualizada """
 
-""" 6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por pantalla los dos primeros. """
 
-""" lista_diez_treinta = list(range(10,31,5))
-print (lista_diez_treinta[:2]) """
-
-""" 7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores cualesquiera.
-autos = ["sedan", "polo", "suran", "gol"]
-""" """
-autos = ["sedan", "polo", "suran", "gol"]
-autos[1] = "fiesta"
-autos[2] = "taos"
-print (autos)
-"""
-
-""" 8) Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append directamente. Imprimir la lista resultante por pantalla."""
-
-""" dobles = []
-dobles.append(5*2)
-dobles.append(10*2)
-dobles.append(15*2)
-print(dobles) """
-
-""" 9) Dada la lista “compras”, cuyos elementos representan los productos comprados por
-diferentes clientes:
-compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
-a) Agregar "jugo" a la lista del tercer cliente usando append.
-b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
-c) Eliminar "pan" de la lista del primer cliente.
-d) Imprimir la lista resultante por pantalla
-"""
-""" compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
-compras[2].append("jugo")
-compras[1][1] = "tallarines"
-compras[0].remove("pan")
-print (compras) """
-
-""" 10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
-● Posición lista_anidada[0]: 15
-● Posición lista_anidada[1]: True
-● Posición lista_anidada[2][0]: 25.5
-● Posición lista_anidada[2][1]: 57.9
-● Posición lista_anidada[2][2]: 30.6
-● Posición lista_anidada[3]: False
-Imprimir la lista resultante por pantalla.
-"""
-
-""" lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
-print(lista_anidada) """
